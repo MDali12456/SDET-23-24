71221b904c58440834a90616ad66400d4931a38f
MDali12456 <mohammedali99335@gmail.com>
2021-12-16 12:38:51 +0530

    Sample Program

---
+++ Sample/src/test/java/GitSample/Test.java
@@ -0,0 +1,13 @@
+package GitSample;
+
+public class Test{
+	public static void main(String[] args) {
+		int a=10;
+		int b=20;
+		a=a+b;
+		b=a-b;
+		a=a-b;
+		System.out.println(a);
+		System.out.println(b);
+}
+}
