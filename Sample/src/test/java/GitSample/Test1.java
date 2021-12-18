b914f903f355732ac80046e7088b60d83b3d5bd1
MDali12456 <mohammedali99335@gmail.com>
2021-12-18 10:23:13 +0530

    Test1 from sample

---
+++ Sample/src/test/java/GitSample/Test1.java
@@ -0,0 +1,25 @@
+package GitSample;
+
+public class Test1 {
+
+	public static void main(String[] args) {
+
+		int n=153;
+		int temp=n;
+		
+		int sum=0;
+		
+		while(n>0)    {
+			int r=n%10;
+			n=n/10;
+			sum=sum+r*r*r;
+			
+		}
+if(temp==sum)
+	System.out.println("Number is Armstrong");
+else {
+	System.out.println("Not an Armstrong");
+}
+	}
+
+}
